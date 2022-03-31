fc023c11b349d3b67b993cf6bad29309836279a1
Weilong Wu <veyron_wu@163.com>
2022-03-31 09:33:55 +0000

    [Eager] Support test_layers's test cases switch to eager mode

--- python/paddle/fluid/dygraph/tracer.py
+++ python/paddle/fluid/dygraph/tracer.py
@@ -64,12 +64,12 @@ final_state_name_mapping = {
         "axis2": "axis2",
         "out": "Out",
     },
-    "one_hot": {
-        "final_op_name": "final_state_one_hot",
-        "x": "X",
-        "num_class": "depth",
-        "out": "Out",
-    }
+    # "one_hot": {
+    #     "final_op_name": "final_state_one_hot",
+    #     "x": "X",
+    #     "num_class": "depth",
+    #     "out": "Out",
+    # }
 }
 
 
